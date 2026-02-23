fd67ad503f30e156bdaca6348a94fc93f7a7da4c
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-02-23 07:27:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+    return (
+        <>
+            <h1>Welcome to React!</h1>
+            <Countries />
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const countries = () => {
+    return (
+        <div>
+            <h1>Countries</h1>
+        </div>
+    );
+};
+
+export default countries;
